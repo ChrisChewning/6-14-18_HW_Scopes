a44c532a85623e354801ae49a927da9cf5bc81a3
Chris <chewning.cl@gmail.com>
2018-06-14 21:24:46 -0500

    commit 4: pythagoras

--- solutions.js
+++ solutions.js
@@ -17,16 +17,66 @@ console.log('howdy');
 // 1. Write a function checkPalindrome that accepts a single argument, a string. The function should return true (Boolean) if the string is a palindrome, false if it is not. Make sure your function will give the correct answer for words with capital letters.
 
 
-const checkPalindrome = (word) => {
-if (word === word.split('').reverse().join('')) {
-  console.log(true);
-} else {
-  console.log(false);
+// const checkPalindrome = (word) => {
+// if (word === word.split('').reverse().join('').toLowerCase()) {
+//   return (true);
+// } else {
+//   return (false);
+// }
+// };
+// console.log(checkPalindrome("anna"));
+// console.log(checkPalindrome("Anna"));
+//
+
+
+// //#3
+// Write a function sumDigits that accepts a number and returns the sum of its digits.
+//
+// console.log(sumDigits(42));
+// => 6;
+
+
+// function sumDigits(addItUp) {
+//   const splitted = addItUp.split('');
+//   console.log(splitted);
+// }
+// sumDigits(42);
+// //
+
+const sumDigits = (addItUp) => {
+  var summed = addItUp.toString().split('').reduce();
+return summed;
 }
-};
-checkPalindrome("anna");
-checkPalindrome("Anna");
+
+console.log(sumDigits(42));
+
+//example from Stack Exchange:
+// var num = 123456;
+// var digits = num.toString().split('');
+// console.log(digits);
 
 
 
-//3
+
+//#4 Pythagoras
+// Write a function calculateSide that takes two arguments: sideA and sideB, and returns the solution for sideC using the Pythagorean theorem.
+//
+// hint: discover the Pythagorean Theorem on a website called google.com
+// hint: checkout the Math methods in javascript
+//
+// console.log(calculateSide(8, 6));
+// // => 10
+//
+const calculateSide = (sideA, sideB) => {
+ var sideC = Math.sqrt(Math.pow(sideA,2) + Math.pow(sideB,2));
+return sideC;
+}
+console.log(calculateSide(8, 6)); //returns 10.
+
+//EXPLANATION:
+
+//Math.sqrt returns the square root of the number. Has 1 parameter.
+//Math.pow returns the base to the power of the exponent. Has 2 parameters.
+
+// console.log(Math.sqrt(9)); //outputs 3.
+// console.log(Math.pow(3,3)); //outputs 27 b.c 3 * 3 * 3
