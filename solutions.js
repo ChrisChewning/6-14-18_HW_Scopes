7948d0ba1177e43bd81fe3a5ac431abe1e9eb9a5
Chris <chewning.cl@gmail.com>
2018-06-14 22:17:21 -0500

    commit 5: sum arrays

--- solutions.js
+++ solutions.js
@@ -43,12 +43,12 @@ console.log('howdy');
 // sumDigits(42);
 // //
 
-const sumDigits = (addItUp) => {
-  var summed = addItUp.toString().split('').reduce();
-return summed;
-}
-
-console.log(sumDigits(42));
+// const sumDigits = (addItUp) => {
+//   var summed = addItUp.toString().split('').reduce();
+// return summed;
+// }
+//
+// console.log(sumDigits(42));
 
 //example from Stack Exchange:
 // var num = 123456;
@@ -67,16 +67,46 @@ console.log(sumDigits(42));
 // console.log(calculateSide(8, 6));
 // // => 10
 //
-const calculateSide = (sideA, sideB) => {
- var sideC = Math.sqrt(Math.pow(sideA,2) + Math.pow(sideB,2));
-return sideC;
-}
-console.log(calculateSide(8, 6)); //returns 10.
+// const calculateSide = (sideA, sideB) => {
+//  var sideC = Math.sqrt(Math.pow(sideA,2) + Math.pow(sideB,2));
+// return sideC;
+// }
+// console.log(calculateSide(8, 6)); //returns 10.
+//
+// //EXPLANATION:
+//
+// //Math.sqrt returns the square root of the number. Has 1 parameter.
+// //Math.pow returns the base to the power of the exponent. Has 2 parameters.
+//
+// // console.log(Math.sqrt(9)); //outputs 3.
+// // console.log(Math.pow(3,3)); //outputs 27 b.c 3 * 3 * 3
+//
+//
+// //#5
+// Write a function sumArray that takes an array as an argument. The array should contain numbers. The function should return the sum of the numbers in the array. Use a for loop within the function to iterate over the array and sum the contents. Use a variable such as
+//
+// let sum = 0;
+// that will accumulate value within the loop. Expected result:
+//
+// console.log(sumArray([1, 2, 3, 4, 5, 6]));
+// => 21
 
-//EXPLANATION:
 
-//Math.sqrt returns the square root of the number. Has 1 parameter.
-//Math.pow returns the base to the power of the exponent. Has 2 parameters.
+const sumArray = (summedItUp) => {
+  let sum = 0;
+  for (let i=0; i < summedItUp.length; i++) {
+    sum += summedItUp[i];
+  }
+  return sum;
+};
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
-// console.log(Math.sqrt(9)); //outputs 3.
-// console.log(Math.pow(3,3)); //outputs 27 b.c 3 * 3 * 3
+
+//HOW TO USE .reduce()?
+
+
+// var total = nums.reduce(function(a,b) {
+//     return (+a)+(+b);
+// });
+//
+// //explanation: .reduce() is an array method.
